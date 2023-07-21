e73591a21694a3bdcff4c65dc3a023d0a1c7a8eb
Kacper Tylzanowski <ktylzanowski@gmail.com>
2023-07-21 13:20:38 +0200

    fix static warning

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -142,9 +142,6 @@ STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
