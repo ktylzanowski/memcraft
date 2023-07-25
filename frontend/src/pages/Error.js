27147cc0189ce1b93619bb4350ec0771f97dc5de
Kacper Tylzanowski <ktylzanowski@gmail.com>
2023-07-25 14:33:46 +0200

    error png

--- frontend/src/pages/Error.js
+++ frontend/src/pages/Error.js
@@ -1,5 +1,16 @@
+import Header from "../components/Layout/Header";
+import Footer from "../components/Layout/Footer"
+import classes from "./Root.module.css"
+import sadsteve from "../images/sadsteve.png"
 const Error = () => {
-  return <h1>Error</h1>;
+  return <>
+  <Header />
+  <main className={classes.main}>
+    <img src={sadsteve} alt="Error" />
+    <h2>Nie znaleziono tej strony :(((</h2>
+  </main>
+  <Footer />
+  </>
 };
 
 export default Error;
