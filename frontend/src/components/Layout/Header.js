6b568b9f1301a0d3e5771654520d36ecfbc93ffd
Kacper Tylzanowski <ktylzanowski@gmail.com>
2023-07-27 19:54:59 +0200

    fix Header

--- frontend/src/components/Layout/Header.js
+++ frontend/src/components/Layout/Header.js
@@ -1,13 +1,13 @@
-import { NavLink } from "react-router-dom";
 import steveface from "../../images/steveface.png";
 import classes from "./Header.module.css";
+import "./Header.module.css";
 import { useContext } from "react";
 import AuthContext from "../../context/AuthContext";
 import ModalMessage from "../../UI/ModalMessage";
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { LinkContainer } from "react-router-bootstrap";
 const Header = () => {
   const { user, logoutUser, authMessage } = useContext(AuthContext);
   let imageUrl = "";
@@ -17,54 +17,47 @@ const Header = () => {
 
   return (
     <>
-    <Navbar expand="lg" className="navbar-dark bg-dark">
-      <Container fluid>
-        <Navbar.Brand>MemCraft</Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: '100px' }}
-            navbarScroll
-          >
-            <Nav.Link>
-              <NavLink
-                to="/"
-              >
-                Losuj Mema
-              </NavLink>
-            </Nav.Link>
+      <Navbar expand="lg" className="navbar-dark custom-navbar">
+        <Container fluid>
+          <Navbar.Brand>MemCraft</Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="me-auto my-2 my-lg-0"
+              style={{ maxHeight: "100px" }}
+              navbarScroll
+            >
+              <LinkContainer to="/">
+                <Nav.Link>Losuj Mema</Nav.Link>
+              </LinkContainer>
+              {user && (
+                <LinkContainer to="/dodajmema">
+                  <Nav.Link>Dodaj mema</Nav.Link>
+                </LinkContainer>
+              )}
+            </Nav>
             {user && (
-              <Nav.Link>
-                <NavLink
-                  to="/dodajmema"
-                >
-                  Dodaj Mema
-                </NavLink>
+              <Nav.Link onClick={logoutUser} style={{ marginRight: "15px" }}>
+                Wyloguj
               </Nav.Link>
             )}
-          </Nav>
-          <Nav.Link style={{ marginRight: '15px'}}>
-          {user && (
-              <NavLink onClick={logoutUser}>
-                Logout
-              </NavLink>
-          )}
-          </Nav.Link>
-          <Nav.Link>
-          <NavLink
-            to={user ? '/konto' : '/authentication'}
-          >
-            <div className={classes.account}>
-              <img src={user ? imageUrl : steveface} alt="icon" className={classes.icon} />
-              {user ? <span>{user.username}</span> : <span>Logowanie</span>}
-            </div>
-          </NavLink>
-          </Nav.Link>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-    {authMessage && <ModalMessage>{authMessage}</ModalMessage>}
+
+            <LinkContainer to={user ? "/konto" : "/authentication"}>
+              <Nav.Link>
+                <div className={classes.account}>
+                  <img
+                    src={user ? imageUrl : steveface}
+                    alt="icon"
+                    className={classes.icon}
+                  />
+                  {user ? <span>{user.username}</span> : <span>Logowanie</span>}
+                </div>
+              </Nav.Link>
+            </LinkContainer>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      {authMessage && <ModalMessage>{authMessage}</ModalMessage>}
     </>
   );
 };
