c59502cd14e8c61ba38ae20fb9cb346d9f85710d
Kacper Tylzanowski <ktylzanowski@gmail.com>
2023-07-31 13:24:19 +0200

    ChangePassword

--- frontend/src/components/Account/ChangePassword.js
+++ frontend/src/components/Account/ChangePassword.js
@@ -1,11 +1,45 @@
-import classes from "./Account.module.css";
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import classes from "./ChangePassword.module.css"
 
-const ChangePassword = () => {
+function ChangePassword() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
     <>
-      <button className={classes.button}>Zmień hasło</button>
+      <Button variant="primary" onClick={handleShow}>
+        Zmień hasło
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title style={{ color: "black" }}>Zmień Hasło</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <form>
+            <input type='password' placeholder='Stare hasło' className={classes.input}>
+            </input>
+            <input type='password' placeholder='Nowe hasło' className={classes.input}>
+            </input>
+            <input type='password' placeholder='Potwierdź nowe hasło' className={classes.input}>
+            </input>
+            <button type='submit' className={classes.button}>
+              Zapisz
+            </button>
+          </form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Zamknij
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </>
   );
-};
+}
 
 export default ChangePassword;
