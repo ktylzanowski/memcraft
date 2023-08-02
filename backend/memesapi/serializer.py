f55dabd7e0e583ae569ce4e43a5e878d52a5cdf2
Kacper Tylzanowski <ktylzanowski@gmail.com>
2023-08-02 13:11:07 +0200

    validate_meme_image

--- backend/memesapi/serializer.py
+++ backend/memesapi/serializer.py
@@ -1,9 +1,23 @@
 from rest_framework import serializers
 from . models import *
+from PIL import Image
+from io import BytesIO
 
 class MemeSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Meme
-        fields = '__all__'
+        fields = ['pk', 'title', 'meme_image', 'author', 'if_accepted']
+
+    def validate_meme_image(self, value):
+        try:
+            image = Image.open(value)
+            image.verify()
+        except Exception as e:
+            raise serializers.ValidationError('To nie jest obraz lub gif.')
         
+        allowed_extensions = ['.jpg', '.jpeg', '.png', '.gif']
+        if not value.name.lower().endswith(tuple(allowed_extensions)):
+            raise serializers.ValidationError('Dozwolone są tylko pliki w formacie JPG, JPEG, PNG lub GIF.')
+
+        return value
