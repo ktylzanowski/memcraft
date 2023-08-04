5207164d38089587547488fb21da4aaad3867b2b
Kacper Tylzanowski <ktylzanowski@gmail.com>
2023-08-04 19:43:14 +0200

    fix likes inf

--- frontend/src/UI/LikeIcon.js
+++ frontend/src/UI/LikeIcon.js
@@ -1,7 +1,40 @@
-import classes from "./LikeIcon.module.css"
+import classes from "./LikeIcon.module.css";
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+import Tooltip from "react-bootstrap/Tooltip";
 
 const LikeIcon = (props) => {
-  return <img src={props.src} alt={props.alt} onClick={props.onClick} className={classes.img}/>;
+  const token = JSON.parse(localStorage.getItem("authTokens"));
+
+  const renderTooltip = (props) => (
+    <Tooltip id="button-tooltip" {...props}>
+      Musisz być zalogowany!
+    </Tooltip>
+  );
+  return (
+    <>
+      {!token ? (
+        <OverlayTrigger
+          placement="right"
+          delay={{ show: 250, hide: 400 }}
+          overlay={renderTooltip}
+        >
+          <img
+            src={props.src}
+            alt={props.alt}
+            onClick={props.onClick}
+            className={classes.img}
+          />
+        </OverlayTrigger>
+      ) : (
+        <img
+          src={props.src}
+          alt={props.alt}
+          onClick={props.onClick}
+          className={classes.img}
+        />
+      )}
+    </>
+  );
 };
 
 export default LikeIcon;
