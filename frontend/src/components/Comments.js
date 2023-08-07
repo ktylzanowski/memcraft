8331f4e8a7ef10e88d3805f581ea3db7e2fdc15d
Kacper Tylzanowski <ktylzanowski@gmail.com>
2023-08-07 18:02:21 +0200

    css comments

--- frontend/src/components/Comments.js
+++ frontend/src/components/Comments.js
@@ -1,18 +1,21 @@
-import classes from "./Comments.module.css"
+import classes from "./Comments.module.css";
 
 const Comments = (props) => {
-  
   return (
-    <div>
+    <div className={classes.container}>
       <h3>Komentarze:</h3>
       {props.comments && props.comments.length > 0 ? (
         props.comments.map((comment) => (
-          <div key={comment.id}>
-            <p>
-            <img src={`http://127.0.0.1:8000/media/icons/${comment.author_icon}`}  className={classes.icon} />
-            {comment.author_username}
-            </p>
-            {comment.text}
+          <div key={comment.id} className={classes.comment}>
+            <div className={classes.user}>
+              <img
+                src={`http://127.0.0.1:8000/media/icons/${comment.author_icon}`}
+                className={classes.icon}
+                alt="Ikona"
+              />
+              {comment.author_username}
+            </div>
+            <div className={classes.text}>{comment.text}</div>
           </div>
         ))
       ) : (
