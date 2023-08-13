aff3b9118f76dc81a529fe4fdde4ab04ab869b9a
Kacper Tylzanowski <ktylzanowski@gmail.com>
2023-08-13 22:19:58 +0200

    minor changes

--- frontend/src/pages/DetailMemePage.js
+++ frontend/src/pages/DetailMemePage.js
@@ -14,20 +14,12 @@ const DetailMemePage = () => {
 };
 
 export async function loader({ params }) {
-  const token = JSON.parse(localStorage.getItem("authTokens"));
-
-  const headers = {
-    "Content-Type": "application/json",
-  };
-
-  if (token) {
-    headers["Authorization"] = `Bearer ${token.access}`;
-  }
-
   try {
     const response = await fetch(`http://127.0.0.1:8000/meme/${params.id}/`, {
       method: "GET",
-      headers: headers,
+      headers: {
+        "Content-Type": "application/json",
+      },
     });
 
     if (response.ok) {
@@ -38,7 +30,7 @@ export async function loader({ params }) {
     }
   } catch {
     throw json(
-      { message: "Coś poszło nie tak! Przepraszamy!." },
+      { message: "Coś poszło nie tak. Przepraszamy!" },
       {
         status: 500,
       }
