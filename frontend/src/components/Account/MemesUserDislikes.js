b413fb8eb03d10ebe619eeb4fe0c7d18b4a95ab3
Kacper Tylzanowski <ktylzanowski@gmail.com>
2023-09-06 10:33:02 +0200

    minor changes

--- frontend/src/components/Account/MemesUserDislikes.js
+++ frontend/src/components/Account/MemesUserDislikes.js
@@ -1,4 +1,4 @@
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, Link } from "react-router-dom";
 import classes from "./UserMemes.module.css";
 import Likes from "../Memes/Likes";
 
@@ -7,25 +7,29 @@ const MemesUserDislikes = () => {
 
   return (
     <>
-      {data.length > 0 ? (
-        data.map((meme) => (
-          <div className={classes.memeContainer} key={meme.id}>
-            <img src={meme.meme_image} alt={meme.title} />
-            <div className={classes.memeInfo}>
-              <h2>{meme.title}</h2>
-              <Likes
-                total_likes={meme.total_likes}
-                total_dislikes={meme.total_dislikes}
-                id={meme.id}
-                ifLike={meme.if_like}
-                ifDislike={meme.if_dislike}
-              />
-            </div>
-          </div>
-        ))
-      ) : (
-        <p className={classes.NoMemes}>Brak zdislikowanych memów!</p>
-      )}
+      <div className={classes.UserMemes}>
+        {data.length > 0 ? (
+          data.map((meme) => (
+            <Link to={`/meme/${meme.id}`} key={meme.id}>
+              <div className={classes.memeContainer} key={meme.id}>
+                <img src={meme.meme_image} alt={meme.title} />
+                <div className={classes.memeInfo}>
+                  <h2>{meme.title}</h2>
+                  <Likes
+                    total_likes={meme.total_likes}
+                    total_dislikes={meme.total_dislikes}
+                    id={meme.id}
+                    ifLike={meme.if_like}
+                    ifDislike={meme.if_dislike}
+                  />
+                </div>
+              </div>
+            </Link>
+          ))
+        ) : (
+          <p className={classes.NoMemes}>Brak zdislikowanych memów!</p>
+        )}
+      </div>
     </>
   );
 };
