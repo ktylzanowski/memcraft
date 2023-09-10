2185ff92df437ce9e541f3112a4d567cb97a5310
Kacper Tylzanowski <ktylzanowski@gmail.com>
2023-09-10 20:42:06 +0200

    minor changes

--- frontend/src/UI/IconUI.js
+++ frontend/src/UI/IconUI.js
@@ -1,7 +1,14 @@
 import classes from "./IconUI.module.css";
 
 const IconUI = (props) => {
-  return <img src={props.src} alt="icon" className={classes.icon} onClick={props.onClick}/>;
+  return (
+    <img
+      src={props.src}
+      alt="icon"
+      className={classes.icon}
+      onClick={props.onClick}
+    />
+  );
 };
 
 export default IconUI;
