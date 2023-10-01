0bd74acb5b0b08c10ab0981bc7a55e5671c386b8
Kacper Tylzanowski <ktylzanowski@gmail.com>
2023-10-01 14:53:11 +0200

    urlpatterns

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -2,9 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("memesapi.urls")),
     path("accounts/", include("accounts.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
+urlpatterns += staticfiles_urlpatterns()
